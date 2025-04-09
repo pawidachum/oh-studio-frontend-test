2eff47781fde65f24001e6e0d3885fcd0bfc1e14
pawida <pawida.may@gmail.com>
2025-04-09 22:30:50 +0700

    feat(home): call api to get list of cats

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
+import { NavLink, useFetcher, useLoaderData } from "react-router";
 import type { Route } from "./+types/home";
+import { log } from "node:console";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,7 +9,28 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+export async function loader() {
+  const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+  if (!response.ok) throw new Error("Failed to fetch cats");
+  return response.json();
+}
+
 export default function Home() {
+  const categoryButtons = [
+    { name: "Expertise", path: "/" },
+    { name: "Branding", path: "/" },
+    { name: "Product", path: "/" },
+    { name: "DesignSystems", path: "/"},
+  ];
+
+//   const cats = useFetcher();
+// const response =  cats.load("https://api.thecatapi.com/v1/images/search?limit=10");
+
+// console.log("response ",response);
+
+const cats = useLoaderData() as {url: string}[];
+console.log("cats ",cats);
+
   return (
     <>
         <div className="flex flex-col items-center justify-center h-screen leading-20 ">
@@ -15,21 +38,33 @@ export default function Home() {
             A brand and product designer working with clients globally
           </p>
 
-          <div className="flex items-center justify-center mt-30 gap-6">
-            <button className="bg-amber-500 rounded-full py-10 shadow-sm">
-              Expertise
-            </button>
-            <button className="bg-primary rounded-full p-1 w-fit shadow-sm">
-              Branding
-            </button>
-            <button className="bg-amber-500 rounded-full p-1 w-fit shadow-sm">
-              product
-            </button>
-            <button className="bg-amber-500 rounded-full p-1 w-fit shadow-sm">
-              Design Systems
-            </button>
-          </div>
+          <nav className="flex rounded-full w-82 h-12 items-center justify-center gap-4">
+      {categoryButtons.map((button) => (
+        <NavLink
+          key={button.name}
+          to={button.path}
+          end
+          className={({ isActive }) =>
+            `px-6 py-3 w-24 h-10 bg-[#e8e5e480] rounded-full  flex items-center text-sm justify-center transition-colors ${
+              isActive ? "bg-white" : "hover:bg-white/50"
+            }`
+          }
+        >
+          {button.name}
+        </NavLink>
+      ))}
+      </nav>
         </div>
+      <div className="grid grid-cols-2 gap-4 mt-10">
+  {cats.map((cat, index) => (
+        <img
+          key={index}
+          src={cat.url}
+          alt={`Cat ${index}`}
+          className="rounded-lg shadow-md"
+        />
+      ))}
+      </div>
     </>
   );
 }
