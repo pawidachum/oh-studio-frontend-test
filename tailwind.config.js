fa3b667058765b5609d05159e3043a2baa4bec8e
pawida <pawida.may@gmail.com>
2025-04-10 22:23:48 +0700

    config: add font family (Roboto)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
       extend: {
-
+        fontFamily: {
+          sans: ['Roboto', 'ui-sans-serif', 'system-ui', 'sans-serif']
+        }
       },
     },
     plugins: [],
