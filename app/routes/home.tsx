789496ade90d65dbe026a019c4a6c990fb2448d6
pawida <pawida.may@gmail.com>
2025-04-10 15:22:33 +0700

    refactor(home): update cat image grid layout with responsive sizing and lazy loading

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -10,7 +10,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader() {
-  const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+  const response = await fetch(
+    "https://api.thecatapi.com/v1/images/search?limit=10"
+  );
   if (!response.ok) throw new Error("Failed to fetch cats");
   return response.json();
 }
@@ -20,50 +22,47 @@ export default function Home() {
     { name: "Expertise", path: "/" },
     { name: "Branding", path: "/" },
     { name: "Product", path: "/" },
-    { name: "DesignSystems", path: "/"},
+    { name: "DesignSystems", path: "/" },
   ];
 
-//   const cats = useFetcher();
-// const response =  cats.load("https://api.thecatapi.com/v1/images/search?limit=10");
-
-// console.log("response ",response);
-
-const cats = useLoaderData() as {url: string}[];
-console.log("cats ",cats);
+  const cats = useLoaderData() as { url: string }[];
 
   return (
     <>
-        <div className="flex flex-col items-center justify-center h-screen leading-20 ">
-          <p className="text-5xl sm:text-6xl px-20 text-center leading-20">
-            A brand and product designer working with clients globally
-          </p>
+      <div className="flex flex-col items-center justify-center leading-20">
+        <p className="text-5xl sm:text-6xl px-20 text-center leading-20">
+          A brand and product designer working with clients globally
+        </p>
 
-          <nav className="flex rounded-full w-82 h-12 items-center justify-center gap-4">
-      {categoryButtons.map((button) => (
-        <NavLink
-          key={button.name}
-          to={button.path}
-          end
-          className={({ isActive }) =>
-            `px-6 py-3 w-24 h-10 bg-[#e8e5e480] rounded-full  flex items-center text-sm justify-center transition-colors ${
-              isActive ? "bg-white" : "hover:bg-white/50"
-            }`
-          }
-        >
-          {button.name}
-        </NavLink>
-      ))}
-      </nav>
-        </div>
-      <div className="grid grid-cols-2 gap-4 mt-10">
-  {cats.map((cat, index) => (
-        <img
-          key={index}
-          src={cat.url}
-          alt={`Cat ${index}`}
-          className="rounded-lg shadow-md"
-        />
-      ))}
+        <nav className="flex rounded-full w-82 h-12 items-center justify-center gap-4">
+          {categoryButtons.map((button) => (
+            <NavLink
+              key={button.name}
+              to={button.path}
+              end
+              className={({ isActive }) =>
+                `px-6 py-3 w-24 h-10 bg-[#e8e5e480] rounded-full  flex items-center text-sm justify-center transition-colors ${
+                  isActive ? "bg-white" : "hover:bg-white/50"
+                }`
+              }
+            >
+              {button.name}
+            </NavLink>
+          ))}
+        </nav>
+      </div>
+      <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {cats.map((cat, index) => (
+          <div key={index} className="w-full h-60 overflow-hidden rounded-lg">
+            <img
+              key={index}
+              src={cat.url}
+              alt={`Cat ${index}`}
+              className="rounded-lg w-full h-full object-cover"
+              loading="lazy"
+            />
+          </div>
+        ))}
       </div>
     </>
   );
