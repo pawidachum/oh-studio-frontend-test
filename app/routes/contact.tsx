19c8beac3af2499a56e4ba3c49ae80ca3f826ed0
pawida <pawida.may@gmail.com>
2025-04-11 20:07:18 +0700

    feat(contact): update styled layout and additional text content

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,5 +1,10 @@
 function Contact() {
-  return <div>contact</div>;
+  return (
+      <div className="flex flex-col items-center justify-center h-100 text-7xl">
+        <p>Let's work together.</p>
+        <p className="text-gray-400">Get in touch</p>
+      </div>
+  );
 }
 
 export default Contact;
