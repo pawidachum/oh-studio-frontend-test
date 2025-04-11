5dc72d80eecd9353172f65f60f6a238f792b63b0
pawida <pawida.may@gmail.com>
2025-04-11 11:24:00 +0700

    feat(welcom): completed welcome component

--- app/components/form/WelcomeStep.tsx
+++ app/components/form/WelcomeStep.tsx
@@ -12,16 +12,19 @@ function WelcomeStep({ onNext }: WelcomeStepProps) {
       <div className="flex items-center">
         <button
           onClick={onNext}
-          className="flex items-center justify-center bg-[#64748c] text-white px-6 py-2 rounded-md hover:bg-[#748499]"
+          className="flex items-center justify-center bg-[#64748c] text-white px-4 py-2 rounded-xl hover:bg-[#748499] hover:ring-[#748499] ring-2 ring-[#64748c] border border-[#64748c] ring-offset-2 ring-offset-white shadow-inner"
         >
           Next
         </button>
-          <span className="ml-2 text-[#64748c]">Press Enter ↵</span>
+        <span className="ml-2 text-[#64748c]">Press Enter ↵</span>
       </div>
 
-      <div className="mt-6 text-center text-gray-500 text-sm">
-        <p>Powered by Formbricks</p>
-        <div className="w-full h-1 bg-gray-300 mt-2"></div>
+      <div className="mt-6 text-center text-[#64748c] text-sm">
+        <p>
+          <span className="text-[#95a0b0]">Powered by</span>{" "}
+          <span className="font-bold">Formbricks</span>
+        </p>
+        <div className="w-full h-2 bg-[#64748c] mt-2 rounded-full"></div>
       </div>
     </div>
   );
