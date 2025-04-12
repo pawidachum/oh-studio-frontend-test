2393c50498e27b893244d69695e1277dd1748fba
pawida <pawida.may@gmail.com>
2025-04-12 14:47:27 +0700

    chore: remove unused code

--- app/components/form/CompanySizeStep.tsx
+++ app/components/form/CompanySizeStep.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 interface CompanySizeStepProps {
   onNext: () => void;
   onBack: () => void;
--- app/components/form/SourceStep.tsx
+++ app/components/form/SourceStep.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 interface SourceStepProps {
   onNext: () => void;
